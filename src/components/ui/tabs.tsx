139634f217baf451527a49b21a9e104e192894b6
Jhonathan <jhonathan_galhardo@hotmail.com>
2025-02-16 21:45:59 -0300

    fix type

--- src/components/ui/tabs.tsx
+++ src/components/ui/tabs.tsx
@@ -128,7 +128,7 @@ const Tab: FC<TabProps> = ({ children, className, value, onClick }) => {
 
 const TabPanel: FC<TabProps> = ({ children, value, className }) => {
   const { active } = useTabsContext();
-  if (value !== active.item) return;
+  if (value !== active.item) return null;
   return (
     <div className={cn("mt-2 rounded-md bg-slate-100 px-6 py-4 shadow-md dark:bg-zinc-900", className)}>{children}</div>
   );
