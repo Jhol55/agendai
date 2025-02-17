aa99043fb480726a7ebb8b76cb9a942df50a6e2c
Jhonathan <jhonathan_galhardo@hotmail.com>
2025-02-16 21:37:35 -0300

    fix notification type

--- src/components/ui/notifications/notifications.tsx
+++ src/components/ui/notifications/notifications.tsx
@@ -8,11 +8,14 @@ import { Loading } from "../loading/loading"
 import { Typography } from "../typography"
 import { Separator } from "../separator"
 
-
+interface NotificationProps {
+  title: string;
+  type: "scheduling" | "payment";
+}
 
 export function Notifications() {
 
-  const [notifications, setNotifications] = useState([]);
+  const [notifications, setNotifications] = useState<NotificationProps[]>([]);
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
@@ -26,7 +29,8 @@ export function Notifications() {
   }, [notifications])
 
   const icons = {
-    scheduling: <IconCalendar className="h-5 w-5" />
+    scheduling: <IconCalendar className="h-5 w-5" />,
+    payment: ""
   }
 
   const [isOpened, setIsOpened] = useState(false);
@@ -78,69 +82,6 @@ export function Notifications() {
   )
 }
 
-function BellIcon(props) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path d="M6 8a6 6 0 0 1 12 0c0 7 3 9 3 9H3s3-2 3-9" />
-      <path d="M10.3 21a1.94 1.94 0 0 0 3.4 0" />
-    </svg>
-  )
-}
 
 
-function CalendarIcon(props) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path d="M8 2v4" />
-      <path d="M16 2v4" />
-      <rect width="18" height="18" x="3" y="4" rx="2" />
-      <path d="M3 10h18" />
-    </svg>
-  )
-}
 
-
-function TruckIcon(props) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path d="M14 18V6a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2v11a1 1 0 0 0 1 1h2" />
-      <path d="M15 18H9" />
-      <path d="M19 18h2a1 1 0 0 0 1-1v-3.65a1 1 0 0 0-.22-.624l-3.48-4.35A1 1 0 0 0 17.52 8H14" />
-      <circle cx="17" cy="18" r="2" />
-      <circle cx="7" cy="18" r="2" />
-    </svg>
-  )
-}
