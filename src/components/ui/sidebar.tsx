f30923a99f62231680a80c6a7b1259efab374889
Jhonathan <jhonathan_galhardo@hotmail.com>
2025-04-24 21:27:55 -0300

    change sidebar to overlap main content

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -113,7 +113,7 @@ export const DesktopSidebar = ({
     <>
       <motion.div
         className={cn(
-          "sticky left-0 h-screen px-4 py-4 hidden md:flex md:flex-col bg-[#2B2D42] dark:bg-neutral-800 w-[60px] flex-shrink-0",
+          "absolute left-0 h-screen px-4 py-4 hidden md:flex md:flex-col bg-[#2B2D42] dark:bg-neutral-800 w-[60px] flex-shrink-0 z-50 border-r border-r-neutral-700",
           "",
           className
         )}
