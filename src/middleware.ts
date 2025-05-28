ed1667e001fe8dd294a0d2e55cf2c91ca08417c3
Jhonathan <jhonathan_galhardo@hotmail.com>
2025-05-28 15:55:52 -0300

    test

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,36 +2,9 @@ import { NextRequest, NextResponse } from 'next/server';
 
 export async function middleware(request: NextRequest) {
   const path = request.nextUrl.pathname;
-  const sessionCookie = request.cookies.get('_chatwoot_session')?.value;
-    console.log(sessionCookie)
-  // Se não tem cookie e não está na página pública ("/"), redireciona
+  const sessionCookie = request.cookies.get('cw_d_session_info')?.value;
+
   if (!sessionCookie && path !== '/') {
-    return NextResponse.redirect(new URL('/', request.url));
-  }
-
-  let isAuthenticated = false;
-
-  if (sessionCookie) {
-    try {
-      const response = await fetch('https://chat.cognic.tech/api/v1/profile', {
-        headers: {
-          Cookie: `_chatwoot_session=${sessionCookie}`,
-        },
-        credentials: 'include',
-      });
-
-      if (response.ok) {
-        isAuthenticated = true;
-      } else {
-        console.warn('Sessão inválida no Chatwoot:', response.status);
-      }
-    } catch (error) {
-      console.error('Erro ao validar sessão do Chatwoot:', error);
-    }
-  }
-
-  // Se o usuário não estiver autenticado, redireciona para "/"
-  if (!isAuthenticated && path !== '/') {
     return NextResponse.redirect(new URL('/', request.url));
   }
 
